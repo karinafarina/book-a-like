f0fd032814e964fe186016184a230bc98a00fdd7
Karina Gaulin <karinagaulin@gmail.com>
2017-04-03 14:45:11 -0400

    Testing

--- app.js
+++ app.js
@@ -7,31 +7,29 @@ $("document").ready(function() {
 			q: searchTerm,
 			page: 2,
 		};
-		console.log("line 10")
-		$.getJSON(FOOD2FORK_BASE_URL,query, function(data, status, z) {
-		console.log("data", data, "status", status, "z", z);
-      	console.log(data.items);
+		console.log("line 10");
+		console.log(searchTerm);
+		$.getJSON(FOOD2FORK_BASE_URL, query, function(data) {
+			console.log(data);
 		})
-	}
+	};
+
+		$("#submit").click(function() {
+			//event.preventDefault();
+			console.log("clicked");
+			getDataFromApi("flax", function() {
+				console.log("callback invoked");
+			})
+		})
+	
 });
 
 
-    
-    /*  for(var i = 0; i < data.items.length; i++) {
-        console.log(data.items[i]);
-        const item = data.items[i]; 
-        if(item.id.kind !== "youtube#channel") {
-          $(".js-search-results").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ item.id.videoId + '" frameborder="0" allowfullscreen>' + item.id.videoId + '"><img class="thumbnail" src="' + item.snippet.thumbnails.default.url + '"></iframe>');
-        } else {
-          console.log("==youtube#channel");
-        } 
-      }
-    });
-  }
+		
 
-  $("button").click(function(event) {
-    event.preventDefault();
-    console.log("clicked");
-    getDataFromApi($("#search-box").val());
-  })
-});*/
+
+
+
+
+
+   
