8913359ca18c076f985b4d764ebcaaff64059936
Karina Gaulin <karinagaulin@gmail.com>
2017-04-10 07:11:36 -0400

    Data.results

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
 $("document").ready(function() {
 	console.log("Here");
-	
-	var FOOD2FORK_BASE_URL = "http://food2fork.com/api/search"; 
+	var TASTEKID_BASE_URL = "https://www.tastekid.com/api/similar?type=books&info&q=";
 	
 	function getDataFromApi(searchTerm, callback) {
-		var query = {
-			key: "d48fb246d87e5aa80bfb9aa7d21cfe84",
-			q: searchTerm,
-			//page: 2,
-		};
-		console.log("line 10");
-		console.log(searchTerm);
-		$.getJSON(FOOD2FORK_BASE_URL, query, function(data) {
-			console.log(data);
-		})
-	};
+		var settings = {
+			url: TASTEKID_BASE_URL+searchTerm, 
+			dataType: "jsonp",
+			jsonpCallback: "logResults",
+			type: 'Get',
+			success: callback
+	 	};
+		$.ajax(settings);
+		
+	}
+	console.log('worked');
 
-		$("#submit").click(function() {
-			//event.preventDefault();
-			console.log("clicked");
-			getDataFromApi("pork", function() {
-				console.log("callback invoked");
+	function displaySearchData(data) {
+		console.log(data);
+		for(var i = 0; i< data.results.length; i++) {
+			console.log(data.results[i]);
+		}
+	}
 
-			})
-		})
-	
+
+	$("#submit").click(function() {
+		//event.preventDefault();
+		console.log("clicked");
+		getDataFromApi($("#search-box").val(), displaySearchData);
+				
+	});
 });
 
 
-		
+
+
+ /*for(var i = 0; i < data.items.length; i++) {
+        console.log(data.items[i]);
+        const item = data.items[i]; 
+        if(item.id.kind !== "youtube#channel") {
+          $(".js-search-results").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ item.id.videoId + '" frameborder="0" allowfullscreen>' + item.id.videoId + '"><img class="thumbnail" src="' + item.snippet.thumbnails.default.url + '"></iframe>');
+        } else {
+          console.log("==youtube#channel");
+        } 
+      }
+    });
+  }
+
+  $("button").click(function(event) {
+    event.preventDefault();
+    console.log("clicked");
+    getDataFromApi($("#search-box").val());
+  })
+});*/
 
 
 
 
 
-
-   
