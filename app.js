b1ea1ee7fcb13118acb831892d7b7becd1f4d95f
Karina Gaulin <karinagaulin@gmail.com>
2017-04-13 12:05:19 -0400

    Add nav bar

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 $("document").ready(function() {
 	console.log("Here");
-	var TASTEKID_BASE_URL = "https://www.tastekid.com/api/similar?type=books&info&q=";
+	var TASTEKID_BASE_URL = "https://www.tastekid.com/api/similar?type=books&info=1&q=";
 	
 	function getDataFromApi(searchTerm, callback) {
 		var settings = {
-			url: TASTEKID_BASE_URL+searchTerm, 
+			url: TASTEKID_BASE_URL+searchTerm,
 			dataType: "jsonp",
 			jsonpCallback: "logResults",
 			type: 'Get',
@@ -15,13 +15,19 @@ $("document").ready(function() {
 	}
 	console.log('worked');
 
+	$("#search-li").click(function() {
+		console.log('search-li clicked');
+		$(this).hide("slow");
+		$('.search-box-container').show('slow');
+	})
+
 	function displaySearchData(data) {
 		console.log(data.Similar.Results);
 		for(var i = 0; i< data.Similar.Results.length; i++) {
 			console.log(data.Similar.Results[i]);
 			
 			var results = data.Similar.Results[i];
-			$("#results").append("<li>" + results.Name + "</li>");
+			$("#results").append("<li>" + results.Name + "</li><br>" + "<li>" + results.wTeaser + "</li><br>");
 		}
 	}
 
@@ -37,26 +43,4 @@ $("document").ready(function() {
 
 
 
- /*for(var i = 0; i < data.items.length; i++) {
-        console.log(data.items[i]);
-        const item = data.items[i]; 
-        if(item.id.kind !== "youtube#channel") {
-          $(".js-search-results").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+ item.id.videoId + '" frameborder="0" allowfullscreen>' + item.id.videoId + '"><img class="thumbnail" src="' + item.snippet.thumbnails.default.url + '"></iframe>');
-        } else {
-          console.log("==youtube#channel");
-        } 
-      }
-    });
-  }
-
-  $("button").click(function(event) {
-    event.preventDefault();
-    console.log("clicked");
-    getDataFromApi($("#search-box").val());
-  })
-});*/
-
-
-
-
-
+ 
